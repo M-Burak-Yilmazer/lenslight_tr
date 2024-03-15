172c236dd6acd1fa381f77cbe340f003a088c388
M Burak Yilmazer <mbyilmazer38@gmail.com>
2024-03-15 16:46:09 +0300

    up

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from "express";
 
-const app = express()
-const PORT = 8080
+const app = express();
+const PORT = 8080;
+app.set("view engine", "ejs");
 
-app.get("/", (req,res)=>res.send("Index Page"))
-app.listen(PORT, ()=>console.log(`Application running on ${PORT}`))
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.render("index"));
+app.get("/about", (req, res) => res.render("about"));
+app.listen(PORT, () => console.log(`Application running on ${PORT}`));
